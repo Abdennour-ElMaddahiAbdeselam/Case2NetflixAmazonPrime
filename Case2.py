b900fc22f6cd1e21cf1da9f754ef355794bc4912
Abdennour-ElMaddahiAbdeselam <abdennour.el.maddahi@hva.nl>
2026-09-21 15:21:26 +0200

    Add files via upload

---
+++ Case2.py
@@ -0,0 +1,11 @@
+import pandas as pd
+import numpy as np
+import matplotlib.pyplot as plt
+import seaborn as sns
+import streamlit as st
+import plotly.express as px
+import os
+import base64
+
+print('hello')
+
